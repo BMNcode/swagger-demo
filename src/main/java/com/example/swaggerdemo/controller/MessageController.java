c5993e430facb10ac45d369a79b62ed56bd74d2c
Максим <happymax@list.ru>
2021-06-21 22:29:42 +0300

    d

--- src/main/java/com/example/swaggerdemo/controller/MessageController.java
+++ src/main/java/com/example/swaggerdemo/controller/MessageController.java
@@ -2,6 +2,11 @@ package com.example.swaggerdemo.controller;
 
 import com.example.swaggerdemo.model.Message;
 import com.example.swaggerdemo.repository.MessageRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,11 +23,21 @@ public class MessageController {
         this.messageRepository = messageRepository;
     }
 
+    @Operation(summary = "gets all message")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Message.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Messages not found",
+                    content = @Content) })
     @GetMapping
     public List<Message> getAllMessage () {
         return messageRepository.findAll();
     }
 
+    @Operation(summary = "create message")
     @PostMapping
     public Message create (@RequestBody Message message) {
         return messageRepository.save(message);
