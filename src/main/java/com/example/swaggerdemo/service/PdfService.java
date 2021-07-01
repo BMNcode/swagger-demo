18fc78557421df49a9b738cb5bd3caa990a7b289
SMT <bulenkovmn@mail.ru>
2021-07-01 10:22:10 +0300

    ref

--- src/main/java/com/example/swaggerdemo/service/PdfService.java
+++ src/main/java/com/example/swaggerdemo/service/PdfService.java
@@ -3,11 +3,9 @@ package com.example.swaggerdemo.service;
 
 import com.lowagie.text.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import java.io.File;
